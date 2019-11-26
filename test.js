5591ba1f002f014bfe56ce8b7e42711dcb54db2a
maleckim <maleckimatthewm@gmail.com>
2019-11-26 13:07:10 -0800

    cleaned

--- test.js
+++ test.js
@@ -11,8 +11,8 @@ const options = {
     })
 };
 
-function nextPage(){
-    $('.locEnter').on('click', function(e){
+function nextPage() {
+    $('.locEnter').on('click', function (e) {
         flyFrom = $('input[class="search"]').val();
         flyFrom = flyFrom.toUpperCase();
         $('.homePage').hide();
@@ -20,42 +20,42 @@ function nextPage(){
     });
 }
 
-function getParams(){
-    $('.params').submit(function(e){
-       event.preventDefault();
-       flyTo = $('.dest').val();
-       flyTo = flyTo.toUpperCase();
-       fromDate = $('.from').val();
-       toDate = $('.to').val();
-       city = $('.city').val();
-       $('.parameterPage').hide();
-       flightFetcher();
-       restaurantFetch(city);
-       weatherFetch();
-       
-          
+function getParams() {
+    $('.params').submit(function (e) {
+        event.preventDefault();
+        flyTo = $('.dest').val();
+        flyTo = flyTo.toUpperCase();
+        fromDate = $('.from').val();
+        toDate = $('.to').val();
+        city = $('.city').val();
+        $('.parameterPage').hide();
+        flightFetcher();
+        restaurantFetch(city);
+        weatherFetch();
+
+
     })
 
 }
 
 
-function genericFetch(url, callback){
+function genericFetch(url, callback) {
 
     fetch(url)
-      .then( response => {
-        if ( response.ok ){
-          return response.json();
-        }
-  
-        throw Error( response.statusText );
-      })
-      .then( responseJSON => {
-        callback( responseJSON );
-      });
-  
-  }
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
 
-function genericFetchOptions(url, callback){
+            throw Error(response.statusText);
+        })
+        .then(responseJSON => {
+            callback(responseJSON);
+        });
+
+}
+
+function genericFetchOptions(url, callback) {
     const options = {
 
         headers: new Headers({
@@ -63,110 +63,84 @@ function genericFetchOptions(url, callback){
         })
     };
 
-    fetch(url,options)
-      .then( response => {
-        if ( response.ok ){
-          return response.json();
-        }
-  
-        throw Error( response.statusText );
-      })
-      .then( responseJSON => {
-        callback( responseJSON );
-      });
-  
-  }
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
 
-function generateFlightURL(){
+            throw Error(response.statusText);
+        })
+        .then(responseJSON => {
+            callback(responseJSON);
+        });
+
+}
+
+function generateFlightURL() {
     let url = `https://api.skypicker.com/flights?fly_from=city:${flyFrom}&fly_to=city:${flyTo}&partner=picky&curr=USD&currency=USD&date_from=${fromDate}&date_to=${toDate}&conversion=USD&partner_market=US&limit=5`
     return url
 }
 
-function displayFlightResults(responseJson){
-    for(let i = 0; i < responseJson.data.length; i ++){
+function displayFlightResults(responseJson) {
+    for (let i = 0; i < responseJson.data.length; i++) {
         $('.test').hide();
         $('.resultsPage').show();
         $('.resultsPage').find('.flightResults').append(`<a target="blank" href=${responseJson.data[i].deep_link}> ${responseJson.data[i].flyFrom}=>${responseJson.data[i].flyTo} </a>`)
     }
 }
 
-  
-function flightFetcher(){
-      let url = generateFlightURL();
-      
-      genericFetch(url, displayFlightResults)
-  }
+
+function flightFetcher() {
+    let url = generateFlightURL();
+
+    genericFetch(url, displayFlightResults)
+}
 
 
-function restaurantCityCallback(responseJson){
-    console.log('id')
+function restaurantCityCallback(responseJson) {
+
     let id = responseJson.location_suggestions[0].city_id
-    
+
     let url = (`https://developers.zomato.com/api/v2.1/search?entity_id=${id}&entity_type=city&q=food&count=5,options`)
 
     genericFetchOptions(url, displayRestaurantResults)
 }
 
-function displayRestaurantResults(responseJson){
-    for(let i = 0; i < 5; i ++){
-        console.log(responseJson.restaurants[i])
+function displayRestaurantResults(responseJson) {
+    for (let i = 0; i < 5; i++) {
+
         $('.resultsPage').find('.restaurantResults').append(`<a target= "blank" href=${responseJson.restaurants[i].restaurant.url}> ${responseJson.restaurants[i].restaurant.name} - ${responseJson.restaurants[i].restaurant.user_rating['aggregate_rating']}*</a>`)
     }
 }
 
-function restaurantFetch(city){
+function restaurantFetch(city) {
     let url = (`https://developers.zomato.com/api/v2.1/locations?query=${city}`)
-    
-    genericFetchOptions(url, restaurantCityCallback )
+
+    genericFetchOptions(url, restaurantCityCallback)
 }
 
-function weatherFetch(){
+function weatherFetch() {
     let url = `http://api.weatherstack.com/current?access_key=ed299871e432d599fc8c3a5bdfa4f859&query=${flyTo}&units=f`
-    
+
 
     genericFetch(url, weatherForecast)
 
 }
 
-function weatherForecast(responseJson){
-    console.log(responseJson);
+function weatherForecast(responseJson) {
+
     $('.resultsPage').find('.weatherResults').append(`<p>Current Temp - ${responseJson.current.temperature}</p><p> Feels Like - ${responseJson.current.feelslike}</p>
     <p>Humidity - ${responseJson.current.humidity}</p><p>${responseJson.current.weather_descriptions}</p>`)
 }
 
 
-// function getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.watchPosition(showPosition);
-//   } else {
-//     x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
-// function showPosition(position) {
-    
-//    let lat = position.coords.latitude;
-//    let long = position.coords.longitude;
-    
-//    genericFetch(`http://api.weatherstack.com/current?access_key=8bbdbccbbeab9c6104711906071d37fe&query=${lat},${long}&units=f`,showWeatherOnMain)
 
-// }
 
-// function showWeatherOnMain(responseJson){
-//     $('.homePage').find('.weather').append(`current temperature is ${responseJson.current.temperature}, let's change that`)   
-// }
 
-function domReady(){
+function domReady() {
     $(nextPage);
     $(getParams);
-    
-    
-    
-          
 }
 
 $(domReady);
-
-
-
-
-
